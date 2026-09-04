9f7fc5ddd128582b9201a545cffbbd32ff855b06
legendlot <legendlot@users.noreply.github.com>
2026-09-04 12:42:39 +0000

    deploy: Manifest (LOT Monorepo) legendlot/throttle@42a5df0b38384624e76570526beed989b73780f3

---
+++ _next/static/chunks/app/not-found-959b89a02705f97d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160],{7522:function(n,e,u){Promise.resolve().then(u.bind(u,1297))},1297:function(n,e,u){"use strict";u.r(e),u.d(e,{default:function(){return c}});var t=u(7653);function c(){return(0,t.useEffect)(()=>{window.location.replace("/")},[]),null}}},function(n){n.O(0,[293,528,744],function(){return n(n.s=7522)}),_N_E=n.O()}]);
